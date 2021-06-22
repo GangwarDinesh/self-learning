41ebfea509cb21ff74e4b8f2b1d1f2b768ccc771
Dinesh Kumar <dineshgngwr@gmail.com>
2021-06-22 16:23:05 +0530

    Add files via upload

---
+++ config-server-1/src/test/java/com/config/ConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
