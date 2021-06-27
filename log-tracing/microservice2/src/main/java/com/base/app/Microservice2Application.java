a9ef002efc7e74139b4639170aa80618791e10a9
Dinesh Kumar <dineshgngwr@gmail.com>
2021-06-27 13:30:44 +0530

    Update and rename Microservic2Application.java to Microservice2Application.java

--- log-tracing/microservice2/src/main/java/com/base/app/Microservice2Application.java
+++ log-tracing/microservice2/src/main/java/com/base/app/Microservice2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Microservic2Application {
+public class Microservice2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Microservic2Application.class, args);
