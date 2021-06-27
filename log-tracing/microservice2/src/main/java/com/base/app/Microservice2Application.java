1ec58af35409dab2f113e27c5b9b46af8cb83ec6
Dinesh Kumar <dineshgngwr@gmail.com>
2021-06-27 13:31:01 +0530

    Update Microservice2Application.java

--- log-tracing/microservice2/src/main/java/com/base/app/Microservice2Application.java
+++ log-tracing/microservice2/src/main/java/com/base/app/Microservice2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Microservice2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Microservic2Application.class, args);
+		SpringApplication.run(Microservice2Application.class, args);
 	}
 
 }
